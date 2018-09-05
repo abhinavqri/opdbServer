660f94f59fdeef215f22cbf0d0c2f5a5b075eb89
abhinavqri <39274385+abhinavqri@users.noreply.github.com>
2018-09-05 11:09:25 -0500

    Update empData.js

--- routes/empData.js
+++ routes/empData.js
@@ -6,7 +6,7 @@ var sql = require("mssql");
 var config = {
     user: 'sa',
     password: 'aP99adr%%M',
-    port: 49762,
+    port: 49893,
     server: 'QRIITSQLDBW01\\QRIITSQLDB',
     database: 'QRIREPORTING',
     options: {
